2448065363ce7ec30ce13d8ba0346b01a38799b7
Gideonke <gideonkitheka15@gmail.com>
2024-04-22 12:08:32 +0300

    added a tailwind

---
+++ components/Router.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from"../pages/Home.jsx"
+
+import Layout from "../pages/Layout.jsx";
+
+import{Link} from"react-router-dom";
+import { useState } from "react";
+function Router(){
+  
+
+    return(<BrowserRouter>
+        <Routes>
+        <Route path="/register"element={<Layout><Register/></Layout>}/>
+        <Route path="/home"element={<Layout><Home/></Layout>}/>
+        <Route path="/login"element={<Layout><Login/></Layout>}/>
+
+
+        </Routes>
+        </BrowserRouter>)
+}
+export default Router;
---
+++ pages/Layout.jsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+import { useState } from "react";
+function Layout() {
+
+  return (
+    <div>
+      <Nav/>
+      <main className="flex min-h-[100vh] flex-col ">{children}</main>
+      <Footer />
+    </div>
+  );
+}
