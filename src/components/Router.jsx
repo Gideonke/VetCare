28824e0258cd4e47b14ceba3b2aff169c382e1bf
Gideonke <gideonkitheka15@gmail.com>
2024-05-02 15:40:01 +0300

    made some deletions

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -5,22 +5,12 @@ import Layout from "../pages/Layout.jsx";
 
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import Login from "../pages/Login.jsx";
-import Register from "../pages/Register.jsx";
-import Booking from "../pages/Booking.jsx";
-import Services from "../pages/Services.jsx";
+
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/register"
-          element={
-            <Layout>
-              <Register />
-            </Layout>
-          }
-        />
+      
         <Route
           path="/"
           element={
@@ -30,32 +20,10 @@ function Router() {
           }
         />
 
-<Route
-          path="/booking"
-          element={
-            <Layout>
-              <Booking />
-            </Layout>
-          }
-        />
 
-<Route
-          path="/services"
-          element={
-            <Layout>
-              <Services/>
-            </Layout>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <Layout>
-              <Login />
-            </Layout>
-          }
-        />
-      </Routes>
+
+
+              </Routes>
     </BrowserRouter>
   );
 }
